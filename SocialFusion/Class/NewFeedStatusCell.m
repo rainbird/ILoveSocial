57ed0e0c7c565f0255f226845385b858d9d21853
shenjunru <shenjunru1992@gmail.com>
2011-11-22 03:25:28 +0800

    沈君儒的初体验

--- SocialFusion/Class/NewFeedStatusCell.m
+++ SocialFusion/Class/NewFeedStatusCell.m
@@ -21,10 +21,10 @@
 @synthesize picView=_picView;
 - (void)awakeFromNib
 {
-    self.defaultHeadImageView.layer.masksToBounds = YES;
+    /*self.defaultHeadImageView.layer.masksToBounds = YES;
     self.defaultHeadImageView.layer.cornerRadius = 5.0f;  
     self.headImageView.layer.masksToBounds = YES;
-    self.headImageView.layer.cornerRadius = 5.0f; 
+    self.headImageView.layer.cornerRadius = 5.0f;*/
    // [self.commentButton setImage:[UIImage imageNamed:@"messageButton-highlight.png"] forState:UIControlStateHighlighted];
 }
 
